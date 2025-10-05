fff8d5ef58e84b40bcff96a440447db3ea4e312d
Swati Samantaray <samantarayswati22@gmail.com>
2025-10-05 21:19:06 +0530

    EDA and Problems statement

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,22 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Create a log filename
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Create only "logs" folder, not with filename
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
 
+# Final log file path inside logs folder
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Logging configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
-
 )
+
+
+
